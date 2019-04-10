5e9e7f34374d2ceab845b7f447341ae7dd669fe5
M Ragul <mragul@mragulc02rpb0lg8wp.local>
2019-04-10 18:45:58 +0530

    Initial Commit

---
+++ ResturantSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResturantSampleApp
+//
+//  Created by M L Ragul on 10/04/19.
+//  Copyright © 2019 M L Ragul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
